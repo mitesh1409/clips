5429fa62ba974bc08ee785dec9e05a889e4b5482
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-04-10 23:04:48 +0530

    Remove code which was added to show the memory leak issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ModalService } from './service/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +6,5 @@ import { ModalService } from './service/modal.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showModal: boolean = false;
-
-  constructor(public modal: ModalService) {
-
-  }
-
-  ngOnInit() {
-    setInterval(
-      () => {
-        this.showModal = !this.showModal;
-        console.log('modals', this.modal.modals);
-      },
-      1000
-    );
-  }
+  constructor() {}
 }
