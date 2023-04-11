d7bec9c3e17894e7696d688bf239b592e7842d98
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-04-11 23:25:08 +0530

    Use ContentChildren decorator to define tabs

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
+  constructor() {}
+
+  ngAfterContentInit(): void {
+    console.log('tabs', this.tabs);
+  }
 }
