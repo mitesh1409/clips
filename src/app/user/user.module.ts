e28bdda4e03f2544c163a349be148054e5850d1a
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-04-12 23:24:39 +0530

    Create login component inside user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
