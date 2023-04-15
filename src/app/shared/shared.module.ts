d180d78a2004af80c4ec79bc1c1e7f257f2dbd2b
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-04-15 18:38:12 +0530

    Generate Input component inside shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 
 
 
@@ -10,7 +11,8 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
